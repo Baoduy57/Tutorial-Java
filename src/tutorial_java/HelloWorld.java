57c125b70ab48e8f361db9e4250229d19314ee13
BaoDuy <thaibaoduy.5724@gmail.com>
2026-05-05 17:56:40 +0700

    init project

---
+++ src/tutorial_java/HelloWorld.java
@@ -0,0 +1,15 @@
+package tutorial_java;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World with Java!");
+
+		System.out.println("Hello World with Java!");
+
+		System.out.println("Hello World with Java!");
+
+		System.out.println("Hello World with Java!");
+
+		System.out.println("Hello World with Java!");
+	}
+}
